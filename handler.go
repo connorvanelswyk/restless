6499f818a18dfabc606e2d0e7536b37a3e012bfe
connorvanelswyk <Meyers2005>
2019-06-17 15:57:33 -0400

    generified

--- handler.go
+++ handler.go
@@ -8,51 +8,27 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/beevik/etree"
+	"golang.org/x/text/encoding/charmap"
 	"io"
 	"net/http"
 	"strings"
 )
 
-type requester struct {
-	ServiceUrl        string                       `json:"serviceUrl"` // The endpoint for the desired SOAP service
-	RequestUrl        string                       `json:"requestUrl"` // The location of an empty or sample SOAP service request
-	RequestMethod     string                       `json:"requestMethod"`
-	RequestProperties map[string]string            `json:"requestProperties"`
-	RequestMap        map[string]string            `json:"requestMap"`
-	ResponseMap       map[string]map[string]string `json:"responseMap"`
-}
-
 // M is an alias for map[string]interface{}
 type M map[string]interface{}
 
-func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func Handler(ctx context.Context, in events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	// unmarshal the proxy request body
-	r := requester{}
-	err := json.Unmarshal([]byte(req.Body), &r)
+	r, err := NewRequester([]byte(in.Body))
 	if err != nil {
 		return newBlankAPIGatewayProxyResponse(), err
 	}
 
-	// get the example service request url
-	resp, err := http.Get(r.RequestUrl)
+	s, err := NewSoapRequestBody(r)
 	if err != nil {
 		return newBlankAPIGatewayProxyResponse(), err
 	}
 
-	// create xml from example service request body
-	reqXml := etree.NewDocument()
-	_, err = reqXml.ReadFrom(resp.Body)
-
-	updateRequestXml(r.RequestMap, *reqXml)
-
-	// write it to a string for request body
-	s, err := reqXml.WriteToString()
-	if err != nil {
-		return newBlankAPIGatewayProxyResponse(), err
-	}
-
-	// create the service request and set the request headers
 	request, err := http.NewRequest(r.RequestMethod, r.ServiceUrl, strings.NewReader(s))
 	if err != nil {
 		return newBlankAPIGatewayProxyResponse(), err
@@ -63,7 +39,7 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 
 	// create client and get response from service
 	client := &http.Client{}
-	resp, err = client.Do(request)
+	resp, err := client.Do(request)
 	if err != nil {
 		return newBlankAPIGatewayProxyResponse(), err
 	}
@@ -93,8 +69,12 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 	for kString, vMap := range r.ResponseMap {
 		for _, e := range xmlquery.Find(xml, "//"+kString) {
 			m := make(map[string]interface{})
-			for k := range vMap {
-				m[k] = xmlquery.FindOne(e, "//"+k).InnerText()
+			if len(vMap) > 0 {
+				for k := range vMap {
+					m[k] = xmlquery.FindOne(e, "//"+k).InnerText()
+				}
+			} else {
+				m[kString] = e.InnerText()
 			}
 			results = append(results, m)
 		}
@@ -103,6 +83,34 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 	// marshal map into json string for response body
 	j, _ := json.Marshal(results)
 	return newAPIGatewayProxyResponse(string(j)), nil
+}
+
+func NewSoapRequestBody(r *Requester) (string, error) {
+	resp, err := http.Get(r.RequestUrl)
+	if err != nil {
+		return "", err
+	}
+	doc := etree.Document{}
+	if r.Encoding == "ISO-8859-1" {
+		doc.ReadSettings = etree.ReadSettings{
+			CharsetReader: CharsetReader,
+		}
+	}
+	_, err = doc.ReadFrom(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	if r.RequestMap != nil {
+		for k, v := range r.RequestMap {
+			e := doc.FindElement("//" + k)
+			e.SetText(v)
+		}
+	}
+	return doc.WriteToString()
+}
+
+func CharsetReader(charset string, input io.Reader) (io.Reader, error) {
+	return charmap.ISO8859_1.NewDecoder().Reader(input), nil
 }
 
 func newBlankAPIGatewayProxyResponse() (r events.APIGatewayProxyResponse) {
@@ -114,17 +122,6 @@ func newAPIGatewayProxyResponse(body string) (r events.APIGatewayProxyResponse) 
 		Body:            body,
 		StatusCode:      200,
 		IsBase64Encoded: false,
-	}
-}
-
-//  update request xml (d) with properties
-//    from request map (m)
-//   where request map keys are element tag names
-//     and request map values are element text values
-func updateRequestXml(m map[string]string, d etree.Document) {
-	for k, v := range m {
-		e := d.FindElement("//" + k)
-		e.SetText(v)
 	}
 }
 
---
+++ requester.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/url"
+	"strings"
+)
+
+type Requester struct {
+	ServiceUrl        string                       `json:"serviceUrl"` // The endpoint for the desired SOAP service
+	RequestUrl        string                       `json:"requestUrl"` // The location of an empty or sample SOAP service request
+	RequestMethod     string                       `json:"requestMethod,omitempty"`
+	Encoding          string                       `json:"encoding,omitempty"`
+	RequestProperties map[string]string            `json:"requestProperties,omitempty"`
+	RequestMap        map[string]string            `json:"requestMap,omitempty"`
+	ResponseMap       map[string]map[string]string `json:"responseMap,omitempty"`
+}
+
+const defaultUserAgent = "Apache-HttpClient/4.1.1"
+const defaultAcceptEncoding = "gzip,deflate"
+
+func NewRequester(inputBody []byte) (*Requester, error) {
+	r := &Requester{}
+	if err := json.Unmarshal(inputBody, r); err != nil {
+		return r, err
+	}
+	if _, err := url.ParseRequestURI(r.ServiceUrl); err != nil {
+		return r, errors.New("service url is malformed")
+	}
+	if _, err := url.ParseRequestURI(r.RequestUrl); err != nil {
+		return r, errors.New("request url is malformed")
+	}
+	if r.RequestProperties == nil {
+		r.RequestProperties = map[string]string{
+			"Host":            Host(r.ServiceUrl),
+			"User-Agent":      defaultUserAgent,
+			"Content-Type":    r.Encoding,
+			"Accept-Encoding": defaultAcceptEncoding,
+		}
+	}
+	if r.Encoding == "" {
+		ct := r.RequestProperties["Content-Type"]
+		if ct != "" && strings.Contains(ct, "ISO-8859-1") {
+			r.Encoding = "ISO-8859-1"
+		} else {
+			r.Encoding = "UTF-8"
+		}
+	}
+	if r.RequestMethod == "" {
+		r.RequestMethod = "POST"
+	}
+	return r, nil
+}
+
+func Host(url string) string {
+	url = strings.Replace(url, "http://", "", -1)
+	url = strings.Replace(url, "https://", "", -1)
+	return url[:strings.Index(url, "/")]
+}
