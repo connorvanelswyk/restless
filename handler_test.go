c2d7c7b349acf20622e790619488c5641f351873
connorvanelswyk <Meyers2005>
2019-06-20 15:50:24 -0400

    improved coverage

--- handler_test.go
+++ handler_test.go
@@ -9,16 +9,12 @@ import (
 var ctx context.Context = nil
 
 var in = events.APIGatewayProxyRequest{
-	Body: "{ " +
-		"\"service\": \"https://graphical.weather.gov:443/xml/SOAP_server/ndfdXMLserver.php\", " +
-		"\"requestBody\": \"https://graphical.weather.gov/xml/docs/SOAP_Requests/LatLonListZipCode.xml\", " +
-		"\"responseMap\": { \"latLonList\": {} } }",
+	Body: "{\"service\": \"https://graphical.weather.gov/xml/SOAP_server/ndfdXMLserver.php\", \"requestBody\": \"https://graphical.weather.gov/xml/docs/SOAP_Requests/GmlLatLonList.xml\", \"requestMap\": {  \"requestedTime\": \"2019-06-22T23:59:59\"  },  \"responseMap\": {    \"gml:boundedBy\": {      \"gml:coordinates\": \"\"    },    \"gml:featureMember\": {      \"gml:coordinates\": \"\",      \"app:validTime\": \"\",      \"app:maximumTemperature\": \"\"    }  }}",
 }
 
 func TestHandler(t *testing.T) {
-	out, err := Handler(ctx, in)
+	_, err := Handler(ctx, in)
 	if err != nil {
 		t.Error("ERROR")
 	}
-	t.Log(out)
 }
